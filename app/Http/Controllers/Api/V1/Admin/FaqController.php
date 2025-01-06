d4f01f12475b63b6bf7e290643f1f049f05f2a2d
afshin <afshin.efati@gmail.com>
2025-01-06 19:43:48 +0330

    faq

--- app/Http/Controllers/Api/V1/Admin/FaqController.php
+++ app/Http/Controllers/Api/V1/Admin/FaqController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\Admin\CreateFaqRequest;
-use App\Http\Requests\Api\Admin\UpdateFaqRequest;
+use App\Http\Requests\Api\V1\Admin\CreateFaqRequest;
+use App\Http\Requests\Api\V1\Admin\UpdateFaqRequest;
 use App\Http\Resources\Api\Admin\FaqResource;
 use App\Models\Faq;
 use App\Services\FaqService;
