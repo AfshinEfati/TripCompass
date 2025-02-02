7fb5e5882085914e8059fb434858a4fa517be44b
afshin <afshin.efati@gmail.com>
2025-02-02 19:20:29 +0330

    remove migration

--- database/migrations/2025_01_28_143958_create_agencies_table.php
+++ database/migrations/2025_01_28_143958_create_agencies_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('agencies', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name_en');
             $table->string('name_fa');
